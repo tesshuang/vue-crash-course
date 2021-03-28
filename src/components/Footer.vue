c5f64736cc337a1315403fcdcc543262cfabed62
Tess Huang <tesshuangxj@yahoo.com>
2021-03-28 16:26:11 -0700

    Add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <p>Copyright @{{ currentYear }}</p>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #333;
+}
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
