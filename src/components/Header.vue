d90114455178b8f7864c7d96b019607bdecb3159
Tess Huang <tesshuangxj@yahoo.com>
2021-03-28 16:39:49 -0700

    Add computed on homepage

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-show="homePage"
       :color="show ? 'red' : 'blue'"
       :text="show ? 'Close' : 'Add task'"
       @btn-click="$emit('show-add-task')"
@@ -20,6 +21,15 @@ export default {
   },
   components: {
     Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
 };
 </script>
