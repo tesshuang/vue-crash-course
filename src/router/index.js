5a3b02de4b53b35ab3218cb6e004129643507907
Tess Huang <tesshuangxj@yahoo.com>
2021-03-28 16:25:54 -0700

    Add vue router to project

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import About from '../views/About';
+import Home from '../views/Home';
+
+const routes = [
+  { path: '/about', name: 'About', component: About },
+  { path: '/', name: 'Home', component: Home },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes, // short for `routes: routes`
+});
+
+export default router;
